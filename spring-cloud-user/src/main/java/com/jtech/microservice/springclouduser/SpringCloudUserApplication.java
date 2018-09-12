6d8c81e1dc080fc3e260857f2d571954e0c18990
BattleHeart <dwlsxj@126.com>
2018-09-12 12:00:23 +0800

    feat：添加用户内容。

---
+++ spring-cloud-user/src/main/java/com/jtech/microservice/springclouduser/SpringCloudUserApplication.java
@@ -0,0 +1,14 @@
+package com.jtech.microservice.springclouduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUserApplication.class, args);
+    }
+}
