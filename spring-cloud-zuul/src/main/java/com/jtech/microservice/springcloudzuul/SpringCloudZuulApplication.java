00bbeec4ea39d327b1b422542363a5eb761067f8
BattleHeart <dwlsxj@126.com>
2018-09-14 11:48:25 +0800

    feat：添加了网关以及监控。

---
+++ spring-cloud-zuul/src/main/java/com/jtech/microservice/springcloudzuul/SpringCloudZuulApplication.java
@@ -0,0 +1,17 @@
+package com.jtech.microservice.springcloudzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关。
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringCloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudZuulApplication.class, args);
+    }
+}
