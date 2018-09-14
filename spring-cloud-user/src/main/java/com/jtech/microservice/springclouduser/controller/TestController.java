770232d28f4c10a333aaf3e4d44c831e653dcefb
BattleHeart <dwlsxj@126.com>
2018-09-14 14:59:07 +0800

    feat:修改配置文件内容。

--- spring-cloud-user/src/main/java/com/jtech/microservice/springclouduser/controller/TestController.java
+++ spring-cloud-user/src/main/java/com/jtech/microservice/springclouduser/controller/TestController.java
@@ -2,6 +2,7 @@ package com.jtech.microservice.springclouduser.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -30,5 +31,13 @@ public class TestController {
      */
     public String fallback() {
         return "fallbck";
+    }
+
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return this.profile;
     }
 }
