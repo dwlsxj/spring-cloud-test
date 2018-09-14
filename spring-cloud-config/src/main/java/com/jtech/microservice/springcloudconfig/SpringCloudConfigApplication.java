a3eadf071b69361f29f6b57b24976202d3ab9517
BattleHeart <dwlsxj@126.com>
2018-09-14 13:54:20 +0800

    feat：添加了关于配置管理的内容。

--- spring-cloud-config/src/main/java/com/jtech/microservice/springcloudconfig/SpringCloudConfigApplication.java
+++ spring-cloud-config/src/main/java/com/jtech/microservice/springcloudconfig/SpringCloudConfigApplication.java
@@ -2,8 +2,13 @@ package com.jtech.microservice.springcloudconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 配置相关信息。
+ */
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigApplication {
 
     public static void main(String[] args) {
