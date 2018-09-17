b11f694bc47bebcd07069fdc0f4c3d717e748c2c
BattleHeart <dwlsxj@126.com>
2018-09-17 11:32:46 +0800

    feat：添加对zipin的支持。

---
+++ spring-cloud-slueth-zipkin/src/test/java/com/jtech/mircroservice/springcloudsluethzipkin/SpringCloudSluethZipkinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jtech.mircroservice.springcloudsluethzipkin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudSluethZipkinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
